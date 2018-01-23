94fa6ca1cdea43557a172e325248c0a21d5dcaa9
MagnusLin <magnuslin@vanchu.net>
2018-01-23 19:06:03 +0800

    add code to describe status 500

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
     Server: require("./src/server"),
-    Client: require("./src/client")
+    Client: require("./src/client"),
+    Error: require('./src/error')
 }
--- src/common/schema_resolver.js
+++ src/common/schema_resolver.js
@@ -39,6 +39,5 @@ module.exports = class {
         }
 
         return this._schemaCache[apiName];
-        
     }
 };
